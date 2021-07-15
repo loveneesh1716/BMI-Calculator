04a3882d1e6083f7dfe2ec6ff97ffa2d2a4ee683
loveneesh bhatnagar <loveneesh1716.cse18@chitkara.edu.in>
2021-07-15 20:49:18 +0530

    Create BMI-Calculator.py

---
+++ BMI-Calculator.py
@@ -0,0 +1,31 @@
+#BMI Calculator
+
+print("\n<<<<<<<<<<<<<-BMI Calculator->>>>>>>>>>>>>\n")
+
+def bmi_calculator(weight, height):
+
+    '''
+        This function is used to calculate bmi of a person.
+        Argunment : weight(float), height(float)
+        Return : bmi(float)
+    '''
+    height = height/100
+    return weight/(height*height)
+
+weight = float(input("Enter your weight in kilograms : "))
+height = float(input("Enter your height in centimeters : "))
+
+bmi = bmi_calculator(weight, height)
+
+print(f'Your BMI is {round(bmi,2)}')
+
+if bmi < 0:
+    print("Please Enter Vaid Details.....") 
+elif bmi < 18.5:
+    print("Underweight.")
+elif 18.5 <= bmi <= 24.9:
+    print("Normal Weight.")
+elif 25.0 <= bmi <= 29.9:
+    print("Overweight.")
+else:
+    print("Obesity.")
